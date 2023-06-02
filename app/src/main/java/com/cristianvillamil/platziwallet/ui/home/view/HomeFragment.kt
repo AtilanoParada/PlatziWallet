296585ef1477f4eefc85299d3b10b7638ec3bda2
AtilanoParada <atilanoparada@gmail.com>
2023-06-02 07:40:59 -0600

    Ejercicios realizados hasta el video 16 del curso de Patzi.

--- app/src/main/java/com/cristianvillamil/platziwallet/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/home/view/HomeFragment.kt
@@ -11,6 +11,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cristianvillamil.platziwallet.R
 import com.cristianvillamil.platziwallet.ui.home.FavoriteTransfer
 import com.cristianvillamil.platziwallet.ui.home.HomeContract
+import com.cristianvillamil.platziwallet.ui.home.data.MessageFactory
+import com.cristianvillamil.platziwallet.ui.home.data.MessageFactory.Companion.TYPE_ERROR
+import com.cristianvillamil.platziwallet.ui.home.data.MessageFactory.Companion.TYPE_SUCCESS
 import com.cristianvillamil.platziwallet.ui.home.presenter.HomePresenter
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -61,5 +64,11 @@ class HomeFragment : Fragment(), HomeContract.View {
 
     override fun showFavoriteTransfers(favoriteTransfer: List<FavoriteTransfer>) {
         favoriteTransferAdapter.setData(favoriteTransfer)
+        val dialogFactory = MessageFactory()
+
+        context?.let {
+            val errorDialog = dialogFactory.getDialog(it, TYPE_ERROR)
+            errorDialog.show()
+        }
     }
 }
